ee4b353aba7be4a36b87231496f1f44dd1f3112d
Geo Mathew Gregory <68841127+dev-sf17@users.noreply.github.com>
2024-11-02 13:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_community.document_loaders import PyPDFLoader
-#from langchain_chroma import Chroma
 from langchain_community.vectorstores import Chroma
 import time
 from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import pandas as pd
 from langchain_community.document_loaders.csv_loader import CSVLoader
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 load_dotenv()
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
@@ -36,7 +31,6 @@ def vector_embedding(docs):
         st.session_state.text_splitter = text_splitter
         st.session_state.final_documents = st.session_state.text_splitter.split_documents(docs)
         st.session_state.vectors = Chroma.from_documents(st.session_state.final_documents, st.session_state.embeddings)
-        #st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
     return st.session_state.vectors.as_retriever()
 
 def pdf_processing(files):
